1e23f7058707c9b4bb494384342cacfdc6bec40c
Ekaterina Morozova <morozova1861@mail.ru>
2022-09-25 22:49:36 +0500

    task3

---
+++ HomeWork1/task3.py
@@ -0,0 +1,9 @@
+x, y = int(input("x = ")), int(input("y = "))
+if y > 0 and x > 0:
+    print("I")
+elif y > 0 and x < 0:
+    print("II")
+elif y < 0 and x < 0:
+    print("III")
+elif y < 0 and x > 0:
+    print("IV")
