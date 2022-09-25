6a983a58b2c13f7d463df87b650fcb156d24cb11
Ekaterina Morozova <morozova1861@mail.ru>
2022-09-25 22:33:47 +0500

    task 2 fixed

--- HomeWork1/task2.py
+++ HomeWork1/task2.py
@@ -2,8 +2,8 @@ value_predikat = (True, False)
 for x in value_predikat:
     for y in value_predikat:
         for z in value_predikat:
-            print(f"¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для значений x = {x}, y = {y}, z = {z}" )
-            if not (x or y or z) == (not x) and (not y) and (not z):
+            print(f"¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для значений x = {x}, y = {y}, z = {z}")
+            if (not (x or y or z)) == ((not x) and (not y) and (not z)):
                 print("Истина")
             else:
                 print("Ложь")
