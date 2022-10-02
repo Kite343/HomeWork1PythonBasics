2b56f9b1428d65f590978ac6b68c2a5a04e53c7b
Ekaterina Morozova <morozova1861@mail.ru>
2022-10-02 11:29:31 +0500

    task3 comment

--- HomeWork3/task3.py
+++ HomeWork3/task3.py
@@ -2,7 +2,7 @@ print("Задайте список целых чисел через пробел
 numsList = input().split()
 # print(numsList)
 # fractionalList = [float('0.' + i.split('.')[1]) if not i.isdigit() else 0.0 for i in numsList] № если 0 всё таки нужен
-fractionalList = [float('0.' + i.split('.')[1]) for i in numsList  if not i.isdigit()] # 0 не учавствует в отборе, если судить по примеру
+fractionalList = [float('0.' + i.split('.')[1]) for i in numsList  if not i.isdigit()] # 0 не учавствует в отборе, если судить по примеру, поэтому дробную часть целых чисел проигнорируем
 # print(fractionalList)
 
 min_el, max_el = fractionalList[0], fractionalList[0]
