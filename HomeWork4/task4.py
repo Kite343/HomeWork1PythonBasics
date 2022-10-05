63f185c2dcec9e8c53b5f326e66d60949295b61e
Ekaterina Morozova <morozova1861@mail.ru>
2022-10-05 23:28:48 +0500

    task4

---
+++ HomeWork4/task4.py
@@ -0,0 +1,28 @@
+def superscript(n):
+    return "".join(["⁰¹²³⁴⁵⁶⁷⁸⁹"[ord(c)-ord('0')] for c in str(n)])
+
+def create_equation():
+    from random import randint as r
+    k = int(input("Задайте степень k для создания уравнения высшей степени\n"))
+    a, b = int(input("Задайте начальную границу промежутка для выбора коэффициентов\n")), int(input("Задайте конечную границу промежутка для выбора коэффициентов\n"))
+    coeffList = [r(a, b) for _ in range(k + 1)]
+    equationStr = ""
+    for i in range(k + 1):
+        if (k - i) > 1:
+            equationStr += str(coeffList[i]) + "x" + superscript(k - i)
+            if coeffList[i + 1] >= 0:
+                equationStr += "+"
+        elif (k - i) == 1:
+            equationStr += str(coeffList[i]) + "x"
+            if coeffList[i + 1] >= 0:
+                equationStr += "+"
+        else:
+            equationStr += str(coeffList[i])
+    equationStr += "=0"
+
+    return equationStr 
+
+for i in range(1, 3):
+    name = "equation" + str(i) + ".txt"
+    with open(name, 'w', encoding='utf-8' ) as f:
+        f.write(create_equation())
